6d574d81029f56486c59e7b45594c84d8753480a
MD FAZAL <72104773+fazal2307@users.noreply.github.com>
2023-12-07 09:34:43 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 let valueInString = String(score)
-console.log( valueInString);
+// console.log( valueInString);
 let valueInNumber = Number(score)
-console.log(valueInNumber)
+// console.log(valueInNumber)
 //"33" =>33
 //"33abc"=>NAN(not a number)
 // true => 1; false =>0
@@ -12,7 +12,19 @@ console.log(valueInNumber)
 
 let isLoggedIn = "fazal"
 let booleanLoggedIn = Boolean(isLoggedIn)
-console.log(booleanLoggedIn);
+// console.log(booleanLoggedIn);
 //1=> true; 0=> false
 //""=> false
 //"fazal"=>true
+
+// ***************************Operations ***********************
+
+let value =3
+let negValue = -value
+// console.log(negVaule)
+console.log(2**3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" +2);
