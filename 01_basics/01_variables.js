3db50ac21d462af0becedd8eaa1b556fa2be2133
MD FAZAL <72104773+fazal2307@users.noreply.github.com>
2023-12-07 08:46:26 +0000

    variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 13345
+let accountName="fazal"
+let accountEmail= "fazalsabhi@google.com"
+let accountPassword = "13456"
+accountCity = "Jaipur"
+
+
+/*
+prefer not to use var
+because of issue in block scope and function scope(s/c)
+*/
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity])
