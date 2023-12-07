ea06140c44f4020cbc8cbbc2a82e11fa8ede6ac1
MD FAZAL <72104773+fazal2307@users.noreply.github.com>
2023-12-07 09:17:11 +0000

    conversionofdatatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInString = String(score)
+console.log( valueInString);
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+//"33" =>33
+//"33abc"=>NAN(not a number)
+// true => 1; false =>0
+
+
+let isLoggedIn = "fazal"
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+//1=> true; 0=> false
+//""=> false
+//"fazal"=>true
