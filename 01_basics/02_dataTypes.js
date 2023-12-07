c0db0a43e9cdc5d91c21e102a99dc8484e23b740
MD FAZAL <72104773+fazal2307@users.noreply.github.com>
2023-12-07 08:47:04 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all js code as newer version
+// alert(3 + 3) // we are using node js not browser
+
+
+
+
+
+
+let fullName = "fazal"
+let age = 23
+let lastName = "anjum"
+
+
+
+
+
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=> stansalone value
+//undefined=>
+//symbol
+
+//object
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
