92798bc599ac5af06907d22381501eb82d87fc52
MD FAZAL <72104773+fazal2307@users.noreply.github.com>
2023-12-13 02:36:46 +0000

    date

--- 01_basics/07_datesInjs.js
+++ 01_basics/07_datesInjs.js
@@ -1,18 +1,20 @@
 // Dates
 
-let myDate = new Date()
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// const myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
 
-// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 0, 23)
+console.log(myCreatedDate.toDateString());
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 // let myCreatedDate = new Date("01-14-2023")
 // // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+// let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
@@ -25,7 +27,7 @@ console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
-    weekday: "long",
+// newDate.toLocaleString('default', {
+//     weekday: "long",
     
-})
+// })
