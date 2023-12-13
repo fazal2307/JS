1e615dbfe440a1561140ab777cdfc9ff6cd4a928
MD FAZAL <72104773+fazal2307@users.noreply.github.com>
2023-12-13 16:51:25 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+// singleton
+// object.create
+
+// object literals
+const JsUSer = {
+    name:"fazal ",
+    age:22,
+    location:"Jaipur",
+    email:"fazalsomething@gmail.com",
+    isLoggedIn:false,
+    lastLoginDaays:["Monday","Saturdaay"]
+}
+console.log(JsUSer.email);
